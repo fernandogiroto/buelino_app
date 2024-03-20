44d5b62963da059086749a74df741bdf4172a1df
fernandogiroto <fernando.giroto@binalogue.com>
2024-03-20 15:08:57 +0100

    Menu

--- resources/js/Layouts/Header.vue
+++ resources/js/Layouts/Header.vue
@@ -56,7 +56,7 @@
                         <span class="nav-link-title">Início</span>
                       </Link>
                     </li>
-                    <li class="nav-item" >
+                    <li class="nav-item d-md-none">
                       <Link :href="route('dashboard')" class="nav-link">
                         <span class="nav-link-icon d-md-none d-lg-inline-block"><IconHome /></span>
                         <span class="nav-link-title">Início</span>
